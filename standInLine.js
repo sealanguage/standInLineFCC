2f205bf1c47f066d803ba19117e34ae654b3a0f9
Elaine Froehlich <elaine.froehlich@gmail.com>
2018-06-27 00:16:27 +0000

    initial commit

---
+++ standInLine.js
@@ -0,0 +1,16 @@
+function nextInLine(arr, item) {
+
+  arr.push(item);
+  var firstElement = arr.shift();
+  return firstElement;
+
+  return item;  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
